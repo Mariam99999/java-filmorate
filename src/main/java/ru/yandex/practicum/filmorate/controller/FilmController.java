dcc011a168108654c9eb82828f650e4531daaeae
Mariam99999 <mariam_agadzhanyan@mail.ru>
2023-04-14 03:35:32 +0400

    fix style

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -22,8 +22,7 @@ public class FilmController {
     public boolean checkValidity(Film film) {
         boolean isValidDescription = film.getDescription().length() <= 200;
         LocalDate localDate = LocalDate.of(1895,
-                12
-                ,
+                12,
                 27);
         boolean isValidDate = film.getReleaseDate().isAfter(localDate);
         boolean isValidDuration = film.getDuration().isPositive();
